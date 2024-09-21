7eb7cfdc31398f30ca9bf6160c25c58f4ed13b79
qrver <www.danil132@gmail.com>
2024-09-22 02:47:04 +0300

    Создание таблицы employees и добавление сотрудника через http://localhost:8000/test_database

--- Module_03/routes/web.php
+++ Module_03/routes/web.php
@@ -11,3 +11,22 @@ use App\Http\Controllers\FormProcessor;
 Route::get('/userform', [FormProcessor::class, 'index']);
 
 Route::post('/store_form', [FormProcessor::class, 'store']);
+
+use App\Models\Employee;
+
+Route::get('/test_database', function () {
+
+    $existingEmployee = Employee::where('email', 'ivan@example.com');
+
+    if ($existingEmployee) {
+        return 'Сотрудник с таким email уже существует!';
+    }
+
+    $employee = new Employee;
+    $employee->first_name = 'Иван';
+    $employee->last_name = 'Иванов';
+    $employee->email = 'ivan@example.com';
+    $employee->save();
+
+    return 'Новый сотрудник сохранен!';
+});
